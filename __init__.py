a2b7032bdb83cdb9cd0d575d364e38ec62eaa370
Case Wright <unconfigured@null.spigotmc.org>
2020-05-12 17:20:30 +0000

    add examples

--- __init__.py
+++ __init__.py
@@ -8,11 +8,18 @@ from .ProgNet import ProgNet
 
 from .ProgBlocks import ProgDenseBlock
 from .ProgBlocks import ProgDenseBNBlock
-#from .ProgBlocks import ProgConv2DBlock
 from .ProgBlocks import ProgMultiDense
 from .ProgBlocks import ProgMultiDenseBN
 from .ProgBlocks import ProgMultiDenseSum
 from .ProgBlocks import ProgMultiDenseConcat
+from .ProgBlocks import ProgLambdaBlock
+from .ProgBlocks import ProgInertBlock
+
+from .ConvBlocks import ProgConv2DBlock
+from .ConvBlocks import ProgConvTranspose2DBNBlock
+from .ConvBlocks import ProgConv2DBNBlock
+from .ConvBlocks import ProgDeformConv2DBlock
+from .ConvBlocks import ProgDeformConv2DBNBlock
 
 from .ProgNetTarget import ProgNetWithTarget
 
